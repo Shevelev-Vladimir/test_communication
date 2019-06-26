68d4cf8928539418d9e23dcc9f42da0b7125a346
Shevelev-Vladimir <51922976+Shevelev-Vladimir@users.noreply.github.com>
2019-06-26 18:30:12 +0500

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,28 +1,25 @@
 # encoding: utf-8
 
-# 
+# Константа с номером версии программы.
 VERSION = 5
 
-#
+# Подключаем файлы из папки lib.
 require_relative "./lib/test"
 require_relative "./lib/inference"
 
-#
-name = ARGV[0]
-
-#
+# Объявляем эксемпляр класса Test.
 test = Test.new
-inference = Inference.new(name)
+inference = Inference.new(ARGV[0])
 
-#
+# Печатаем версию программы.
 inference.print_version(VERSION)
-#
+# Здорованмся с пользователем.
 inference.say_hi
 
-#
+# До тех пока тест запущен, выводим следующий вопрос.
 while test.in_progress?
   test.following_question
 end
 
-#
+# Вывод результата игры.
 inference.print_result(test)
