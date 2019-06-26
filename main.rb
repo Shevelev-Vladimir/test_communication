4d7c70cc1f0cefe01d06567ca2cfac20b9b4a969
Shevelev-Vladimir <51922976+Shevelev-Vladimir@users.noreply.github.com>
2019-06-26 23:19:09 +0500

    Shevelev vladimir patch 2 (#3)
    * Update main.rb
    
    * Add files via upload
    
    * Refactoring.

--- main.rb
+++ main.rb
@@ -1,28 +1,38 @@
 # encoding: utf-8
 
-# 
+# Константа с номером версии программы.
 VERSION = 5
 
-#
-require_relative "./lib/test"
-require_relative "./lib/inference"
+# Подключаем файлы из папки lib.
+require_relative 'lib/test'
+require_relative 'lib/inference'
 
-#
-name = ARGV[0]
+current_path = File.dirname(__FILE__)
+questions_path = current_path + "/data/questions.txt"
+results_path = current_path + "/data/results.txt"
 
-#
-test = Test.new
-inference = Inference.new(name)
+begin
+  # Объявляем экземпляр класса Test.
+  test = Test.new(questions_path)
+  inference = Inference.new(ARGV[0], results_path)
 
-#
-inference.print_version(VERSION)
-#
-inference.say_hi
+  # Печатаем версию программы.
+  inference.print_version(VERSION)
+  # Здороваемся с пользователем.
+  inference.say_hi
 
-#
-while test.in_progress?
-  test.following_question
+  # До тех пока тест запущен, выводим следующий вопрос.
+  while test.in_progress?
+    test.following_question
+  end
+
+  # Вывод результата игры.
+  inference.print_result(test)
+
+rescue SystemCallError => error
+  puts "Не найден файл: " + error.message
+rescue Interrupt
+  puts "\nВы вышли из игры."
+# rescue
+  puts "Что-то пошло не так..."
 end
-
-#
-inference.print_result(test)
